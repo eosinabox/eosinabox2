fc0bb7ab04785bbcb088efbb887491dce0511462
Ami Heines <ami.heines@gmail.com>
2021-10-02 21:26:07 +0300

    fix toast bug?

--- client/src/client.js
+++ client/src/client.js
@@ -645,7 +645,7 @@ $(() => {
   });
   // onLoad
   /////////
-  $('.toast').toast('hide'); // aarg! why is the toast not showing but blocking the elements under it?
+  $('.toast').hide(); // aarg! why is the toast not showing but blocking the elements under it?
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./pwaServiceWorker.js');
   }
